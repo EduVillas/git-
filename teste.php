a02a8ef3b1fc9bc767ab32fd89844d1ec67ecd33
villasweb <villasweb@gmail.com>
2022-04-14 14:50:15 -0300

    Alterado o teste

--- teste.php
+++ teste.php
@@ -1,1 +1,15 @@
-arquivo na v5
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>V 5</title>
+</head>
+
+<body>
+    arquivo atualizado na v5
+</body>
+
+</html>
